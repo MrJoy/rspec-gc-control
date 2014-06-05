9e807516ed7f9fec69776b4378476e1939febbf1
Jon Frisby <jfrisby@mrjoy.com>
2014-06-05 16:15:48 -0700

    Refactor to separate summarization of data from display of it.

--- lib/rspec-gc-control/base_text_formatter.rb
+++ lib/rspec-gc-control/base_text_formatter.rb
@@ -10,16 +10,30 @@ module RSpec
         # count and duration, if explicit GC control is enabled.
         def dump_summary(duration, example_count, failure_count, pending_count)
           super(duration, example_count, failure_count, pending_count)
-          # Don't print out profiled info if there are failures, it just clutters the output
+
+          gc_times  = examples.
+                        map { |ex| ex.execution_result[:gc_time] }.
+                        select { |t| t && (t > 0) }
+          gc_count  = gc_times.count
+          gc_time   = gc_times.reduce(:+)
+
+          dump_summary_to_output(duration, gc_time, gc_count, example_count,
+                                 failure_count, pending_count)
+        end
+
+        protected
+
+        def dump_summary_to_output(duration, gc_time, gc_count, example_count,
+                           failure_count, pending_count)
+          # Don't print out profiled info if there are failures, it just
+          # clutters the output.
           dump_profile if profile_examples? && failure_count == 0
           output.print "\nFinished in #{format_duration(duration)}"
 
-          gc_times = examples.map { |ex| ex.execution_result[:gc_time] }.select { |t| !t.nil? && (t > 0) }
-          if(gc_times.count > 0)
-            gc_count = gc_times.count
-            gc_time = gc_times.reduce(:+)
+          if(gc_count > 0)
             output.print " (including #{gc_count} forced GC cycle(s), totalling #{format_duration(gc_time)})"
           end
+
           output.print "\n"
           output.puts colorise_summary(summary_line(example_count, failure_count, pending_count))
           dump_commands_to_rerun_failed_examples
